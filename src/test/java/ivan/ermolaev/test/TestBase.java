2d4a8a04b26ed3c1df997eef933d89ba002f7964
ErmolaevIvan <thedollmaster@bk.ru>
2022-05-01 12:14:22 +0300

    выбрано разрешение выше, т.к на меньшем тест на удаленной машине падал(локально проходил)

--- src/test/java/ivan/ermolaev/test/TestBase.java
+++ src/test/java/ivan/ermolaev/test/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "2560x1440";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
